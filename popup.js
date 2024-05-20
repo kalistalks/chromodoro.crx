9a470fac0f7aac96e821feb79dfd1d6d9062af6b
kalistalks <62800848+kalistalks@users.noreply.github.com>
2024-05-20 21:15:37 +0800

    functional add task

--- popup.js
+++ popup.js
@@ -1,18 +1,27 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+document.addEventListener("DOMContentLoaded", function() {
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+    const inputButton = document.getElementById("input-button");
 
-function addTask() {
-    const task = inputBox.value.trim();
-    if (!task) {
-        alert("Please enter a task");
-        return;
+    inputButton.addEventListener("click", addTask);
+
+    function addTask() {
+        const task = inputBox.value.trim();
+        if (!task) {
+            alert("Please enter a task");
+            return;
+        }
+
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <label class="me-4">
+                <input type="checkbox" class="task-checkbox">
+                <span>${task}</span>
+            </label>
+            <button class="btn edit-button"><img src="images/edit.svg" alt="edit"></button>
+            <button class="btn delete-button"><img src="images/trash.svg" alt="delete"></button>`;
+        
+        listContainer.appendChild(li);
+        inputBox.value = "";
     }
-    const li = document.createElement("li");
-    li.innerHTML = `<label>
-    <input type="checkbox">
-    <span>${task}</span>
-    </label>
-    <span class="edit-btn"><img src="images/edit.png" alt="edit"></span>
-    <span class="delete-btn"><img src="images/trash.png" alt="delete"></span>`;
-    listContainer.appendChild(li);
-}
+});
