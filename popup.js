41ca07c7227fb813f29b7a89e18806a24ad072ba
kalistalks <62800848+kalistalks@users.noreply.github.com>
2024-05-21 21:10:13 +0800

    checkbox functionality

--- popup.js
+++ popup.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("Please enter a task");
             return;
         }
-
         const li = document.createElement("li");
         li.innerHTML = `
             <label class="me-4">
@@ -23,5 +22,21 @@ document.addEventListener("DOMContentLoaded", function() {
         
         listContainer.appendChild(li);
         inputBox.value = "";
-    }
+
+        const checkbox = li.querySelector("input");
+        const editBtn = li.querySelector(".edit-btn");
+        const taskSpan = li.querySelector("span");
+        const deleteBtn = li.querySelector(".delete-btn");  
+          
+        checkbox.addEventListener("click", function () {
+            if (checkbox.checked) {
+                taskSpan.style.textDecoration = "line-through";
+                taskSpan.style.color = "grey";
+            }
+            else {
+                taskSpan.style.textDecoration = "none";
+                taskSpan.style.color = "black";
+            }
+        });
+    }  
 });
