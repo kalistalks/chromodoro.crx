9ee460d7183c4222388d38c0430b6d3b73348233
kalistalks <62800848+kalistalks@users.noreply.github.com>
2024-06-02 23:18:27 +0800

    Update background.js

--- background.js
+++ background.js
@@ -4,8 +4,20 @@ chrome.alarms.create("pomodoroTimer", {
 
 chrome.alarm.onAlarm.addListener((alarm) => {
   if (alarm.name == "pomodoroTimer") { 
-  
+    chrome.storage.local.get(["timer", "isRunning"], (result) => {
+      if (result.isRunning) {
+        let timer = result.timer + 1
+        chrome.storage.local.set({timer: timer}) 
+      }
+  })
   }
+})
+
+chrome.storage.local.get(["timer", "isRunning"], (result) => {
+  chrome.storage.local.set({
+    timer: "timer" in result ? result.timer : 0,
+    isRunning : "isRunning" in result ? result.isRunning : false 
+  })
 })
 
 let countdown;
