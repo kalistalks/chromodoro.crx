f65cfa72a08573ec71db35a195a606e8f1715213
kalistalks <62800848+kalistalks@users.noreply.github.com>
2024-06-06 15:25:01 +0800

    option page updates

--- options.js
+++ options.js
@@ -1,5 +1,35 @@
-const timeOption = document.getElementById("time-option");
-timeOption.addEventListener("change", (event) => {
+const workOption = document.getElementById("work-option");
+workOption.addEventListener("change", (event) => {
     const value = event.target.value;
-    console.log(value);
+    if (value < 1 || value > 60) { 
+        workOption.value = 25;
+    }
 })
+
+const workButton = document.getElementById("work-button");  
+workButton.addEventListener("click", () => {
+    chrome.local.storage.set({
+        timer: 0,
+        workOption: workOption.value,
+        isRunning: false,
+    })
+});
+
+const breakOption = document.getElementById("break-option");
+breakOption.addEventListener("change", (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 60) { 
+        breakOption.value = 5;
+    }
+})
+
+const breakButton = document.getElementById("break-button");  
+workButton.addEventListener("click", () => {
+    chrome.local.storage.set({
+        timer: 0,
+        breakOption: breakOption.value,
+        isRunning: false,
+    })
+});
+
+chrome.storage.local.get()
